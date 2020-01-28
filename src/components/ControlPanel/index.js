ac4d98f1bda968a7f66bb7705eee0f0f8c8bce08
ARochniak <15bblinn@gmail.com>
2020-01-28 17:50:58 +0200

    ESLint fixes

--- src/components/ControlPanel/index.js
+++ src/components/ControlPanel/index.js
@@ -1,11 +1,24 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { setResultsPerPage } from '../../store/thunk';
+import { setResultsPerPage, setPage } from '../../store/thunk';
 import AddPopup from '../AddPopup';
 
 import './index.css';
 
-const ControlPanel = ({ title, dispatch }) => {
+const ControlPanel = ({ title, dispatch, courses, users, resultsPerPage }) => {
+  const searchInput = React.createRef();
+  const searchSubmit = e => {
+    e.preventDefault();
+    const items = title === 'courses' ? courses : users;
+    const searchedItemIdx = items.findIndex(
+      item =>
+        item.name.toLowerCase() === searchInput.current.value.toLowerCase()
+    );
+    if (!(searchedItemIdx + 1)) return false;
+    const page = Math.ceil((items.length - searchedItemIdx) / resultsPerPage);
+    dispatch(setPage(title, page));
+
+  };
   const [isAddPopup, showAddPopup] = useState(false);
   const addHandler = () => {
     showAddPopup(true);
@@ -23,12 +36,14 @@ const ControlPanel = ({ title, dispatch }) => {
           <button className="add" type="button" onClick={addHandler}>
             +
           </button>
-          <div className="find">
-            <button className="find__button" type="button">
-              {' '}
-            </button>
-            <input className="find__search" placeholder="search" />
-          </div>
+          <form className="find" onSubmit={searchSubmit}>
+            <input className="find__button" type="submit" value="" />
+            <input
+              className="find__search"
+              ref={searchInput}
+              placeholder="search by name"
+            />
+          </form>
         </div>
         <h2 className="control-panel__title">{title.toUpperCase()}</h2>
         <div className="control-panel__select">
@@ -43,4 +58,12 @@ const ControlPanel = ({ title, dispatch }) => {
   );
 };
 
-export default connect()(ControlPanel);
+const mapStateToProps = state => {
+  return {
+    courses: state.courses.list,
+    users: state.users.list,
+    resultsPerPage: state.resultsPerPage
+  };
+};
+
+export default connect(mapStateToProps)(ControlPanel);
